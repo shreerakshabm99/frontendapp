2162c7b95e53ab2fcbb9cc614d9d6dcbd7347a9f
Shreerakshabm99 <shreerakshabm99@gmail.com>
2025-09-12 11:16:25 +0530

    latest update

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -2,21 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import login from './components/login';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
+
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/Login" element={<Login />}/>
-=======
-        <Route path="/" element={<login />} />
+
         {/* <Route path="/home" element={<Home />} />/ */}
->>>>>>> 0b298e9fa9322e8949dd20c926052f7cfae5d445
-      </Routes>
+ </Routes>
     </Router>
   );
 }
